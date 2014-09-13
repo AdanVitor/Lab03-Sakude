17e84df646351244c8c6da0363a7003567143452
Adan Vitor <adanvitor90@gmail.com>
2014-09-13 18:03:45 -0300

    Basic code from Lab 02

---
+++ CCI36L03/Figure.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "PolyFill.h"
+
+
+class Figure{
+public:
+	Figure(){}
+	bool wasSelected(point_type point){}
+
+};
---
+++ CCI36L03/SceneGraph.h
@@ -0,0 +1,3 @@
+#include "Figure.h";
+void insertFigure(Figure figure);
+void deleteFigure(Figure figure);
