d881d00d4951e18101225ffd684b5f6a67d649e7
Victor Franco <victorjtfranco@gmail.com>
2014-09-30 14:54:15 -0300

    New files removed from vcxproj configuration, project compiles now

--- CCI36L03/SceneGraph.cpp
+++
@@ -1,7 +0,0 @@
-#include "stdafx.h"
-#include "Figure.h"
-#include "SceneGraph.h"
-
-SceneGraph::SceneGraph(){}
-void SceneGraph::insertFigure(Figure figure){}
-void SceneGraph::deleteFigure(Figure figure){}
